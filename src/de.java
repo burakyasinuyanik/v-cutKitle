5be282a0931599ec69fa128867c0d30e308e25e7
burak yasin <burakyasin3370@gmail.com>
2022-11-06 15:43:48 +0300

    ok

--- src/de.java
+++ src/de.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class de {
+import java.util.Scanner;
+public class de {
+
+    public static void main(String[] args) {
+
+        double tutar, kdvTutar, kdvliTutar, kdv;
+        boolean kdvDurum;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ücret tutarını giriniz: ");
+        tutar = input.nextDouble();
+
+        kdvDurum = (tutar <= 999) && (tutar >= 1000);
+        kdv = kdvDurum ? 0.18 : 0.8;
+
+        kdvTutar = tutar * kdv;
+        kdvliTutar = kdvTutar + tutar;
+
+        System.out.println("KDV Oranı : " + kdv);
+        System.out.println("KDV'siz Tutar : " + tutar);
+        System.out.println("KDV Tutarı : " + kdvTutar);
+        System.out.println("KDV'li Tutarı: " + kdvliTutar);
+}
 }
--- src/nothesaplama.java
+++ src/nothesaplama.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class test {
+public class nothesaplama {
     public static void main(String[] args) {
 
         int matematik, turkce, fizik, kimya, muzik, tarih;
