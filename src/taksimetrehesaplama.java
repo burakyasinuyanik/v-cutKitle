d8c4b4b0ee772bb54754b91c42295d43d2aa6f6b
burak yasin <burakyasin3370@gmail.com>
2022-11-07 15:15:11 +0300

    ok

--- src/taksimetrehesaplama.java
+++ src/taksimetrehesaplama.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class taksimetrehesaplama {
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class taksimetrehesaplama {
+
+     public static void main(String[] args) {
+
+         Scanner input= new Scanner(System.in);
+
+       Double km,ucretKm, ilkKm ;
+       boolean kmKontrol;
+
+         System.out.print("Lütfen km bilgisi giriniz:"  );
+         ucretKm = input.nextDouble();
+
+         ilkKm = ucretKm*2.20+10;
+         kmKontrol = (0 < ilkKm) && (ilkKm < 20);
+         km = kmKontrol ? 20 : ilkKm ;
+         System.out.println("Ödenecek tutar : " +km);
+
+
+
+    }
+
 }
